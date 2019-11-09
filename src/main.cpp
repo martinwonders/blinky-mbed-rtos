e8326dfbe07759e87bc81d04090bab905ba8c797
martinwonders <martin.wonders@unn.ac.uk>
2019-11-09 03:12:46 -0800

    Exercise 1 completed

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,50 @@
 
 #include <mbed.h>
 
+DigitalOut   rLED_as(D5,1); /* initialise app shield red led off */
 DigitalOut   red(LED1,1); /* initialise to 1 = off */
+DigitalOut   green(LED2,1); /* initialise app shield green led off */
+DigitalOut   blue(LED3,1); /* initialise app shield green led off */
+
 
 void flash_red(void)
 {
 	while(true) {
-		red = !red;
+		red = 0;
 		wait(0.3);
+                red = 1;
+		wait(0.6);
+	}
+}
+
+void flash_green(void)
+{
+	wait(0.3);
+        while(true) {
+		green = 0;
+		wait(0.3);
+                green = 1;
+                wait(0.6);
+	}
+}
+
+void flash_blue(void)
+{
+	wait(0.6);
+        while(true) {
+		blue = 0;
+		wait(0.3);
+                blue = 1;
+		wait(0.6);
 	}
 }
 
 int main(void)
 {
-	Thread blink;
-	blink.start(flash_red);
-
-	blink.join();
+	Thread blink_red, blink_green, blink_blue;
+	blink_red.start(flash_red);
+        blink_green.start(flash_green);
+        blink_blue.start(flash_blue);
+	blink_red.join();
 }
 
